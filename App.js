97e74454e363522cc71ccfbb79b0ff8f422396e7
biznesskit <biznesskitltd@gmail.com>
2021-04-11 17:35:06 +0300

    initial commit

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("APP executed!");
+  let x = 1;
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working!</Text>
       <StatusBar style="auto" />
     </View>
   );
